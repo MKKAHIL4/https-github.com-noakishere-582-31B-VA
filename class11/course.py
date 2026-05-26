e75eec4f87f7f8400ec327aef8ea240b1ac492cb
MKKAHIL4 <muhamed_kahil@hotmail.com>
2026-05-26 11:42:13 -0400

    creating class Course class11 course.py

---
+++ class11/course.py
@@ -0,0 +1,20 @@
+from status import CourseStatus, DeliveryMode
+
+MAX_CAPACITY = 60
+
+class Course:
+    def __init__(self, title, capacity, status, delivery_mode):
+        
+        set.title = title 
+        
+        if capacity <= 0:
+            raise ValueError("Capacity must be greater then 0")
+        
+        if capacity > MAX_CAPACITY:
+            raise ValueError(f"Capacity cannot exceed {MAX_CAPACITY}")
+        
+        self.capacity = capacity
+        
+        self.status = status
+        self.delivery_mode = delivery_mode
+        
