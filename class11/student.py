01dfe170a6b5ef077c8b962934cc38273709ec56
MKKAHIL4 <muhamed_kahil@hotmail.com>
2026-05-26 13:09:19 -0400

    finalizing student with examples  student   class11 student.py

---
+++ class11/student.py
@@ -0,0 +1,33 @@
+from status import StudentLevel
+
+
+class Student:
+    def __init__(self, name, level):
+        self.name = name
+        self.level = level
+
+    @property
+    def level(self):
+        return self.__level
+    
+    @level.setter
+    def level(self, value):
+        
+        if not isinstance(value, StudentLevel):
+            raise ValueError("level must be a Student level value")
+        
+        self.__level = value
+    
+    def display_info(self):
+        print(
+            f"student: {self.name} | "
+            f"level: {self.level.value}"
+        )
+        
+student1 = Student("Alice", StudentLevel.BEGINNER)
+student1.display_info()
+student2 = Student("MK", StudentLevel.INTERMEDIATE)
+student2.display_info()
+student3 = Student("Micheal", StudentLevel.ADVANCED)
+student3.display_info()
+    
