5d48314f74f53f79ecbdc249c92389c7bfaaacf5
MKKAHIL4 <muhamed_kahil@hotmail.com>
2026-06-16 10:19:21 -0400

    reorganize project into models and core folders

--- class17/main.py
+++ class17/main.py
@@ -1,9 +1,11 @@
-from constants import MAX_TICKETS_PER_BOOKING
-from enums import ShowStatus
-from customer import Customer
-from movie_show import MovieShow
+from core.constants import MAX_TICKETS_PER_BOOKING
+from core.enums import ShowStatus
+from models.customer import Customer
+from models.movie_show import MovieShow
+
 
 def main():
+
     customer = Customer("Ava")
     show = MovieShow("Inception", 20, ShowStatus.OPEN)
 
