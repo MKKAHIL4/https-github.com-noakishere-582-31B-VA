eddd4109338d03e8a87396f9fff1ceeecdf910e2
MKKAHIL4 <muhamed_kahil@hotmail.com>
2026-07-21 10:08:50 -0400

    class23

--- class23/models.py
+++ class23/models.py
@@ -6,32 +6,8 @@ from werkzeug.security import (check_password_hash, generate_password_hash)
 # let's start our db
 db = SQLAlchemy()
 
-# Now create the model class
 class Member(UserMixin, db.Model):
-    __tablename__ = "memeber"
-
-#let's define our fields
-id = db.Column(db.Integer,
-primary_key=True)
-
-username = db.Column(db.String(50),
-nullable=False,
-unique=True)
-
-email = db.Column(db.String(255),
-nullable=False,
-unique=True)
-
-password_hash = db.Column(db.String(255),
-nullable=False)
-
-= "member"
-
-# methods
-
-
-class Member(UserMixin, db.Model):
-tablename
+ __tablename__ = "memeber"
 
 #let's define our fields
 id = db.Column(db.Integer,
