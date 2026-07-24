92a184e58321b6a180182571b320d4c94ad06c20
MKKAHIL4 <muhamed_kahil@hotmail.com>
2026-07-24 19:04:47 -0400

    Add user registration

--- Shelf/app.py
+++ Shelf/app.py
@@ -1,4 +1,4 @@
-from flask import Flask 
+from flask import Flask, render_template, request, redirect, url_for, flash 
 from flask_login import LoginManager
 from models import db, User
 
@@ -7,7 +7,7 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = "shelf-secret-key"
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///shelf.db"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = db
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)
 
@@ -16,11 +16,34 @@ login_manager.init_app(app)
 
 @login_manager.user_loader
 def load_user(user_id):
-    return user.query.get(init(user_id))
+    return User.query.get(int(user_id))
 
 @app.route("/")
 def home():
     return "<h1>Shelf is Working</h1>"
+    
+@app.route("/register", methods=["GET", "POST"])
+def register():
+
+    if request.method == "POST":
+        username = request.form["username"]
+        email = request.form["email"]
+        password = request.form["password"]
+
+        user = User(
+            username=username,
+            email=email
+            )
+
+        user.set_password(password)     
+        db.session.add(user)
+        db.session.commit()
+
+        flash("Account Created Successfully")
+
+        return redirect(url_for("home"))
+    return render_template("register.html")
+
 
 with app.app_context():
     db.create_all()
