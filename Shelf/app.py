db8430b283bd05a71d54540a275c9305b461157c
MKKAHIL4 <muhamed_kahil@hotmail.com>
2026-07-24 17:32:03 -0400

    Configure flask login

--- Shelf/app.py
+++ Shelf/app.py
@@ -1,11 +1,22 @@
 from flask import Flask 
-from models import db
+from flask_login import LoginManager
+from models import db, User
 
 app = Flask(__name__)
+
+app.config["SECRET_KEY"] = "shelf-secret-key"
+
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///shelf.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = db
 
 db.init_app(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return user.query.get(init(user_id))
 
 @app.route("/")
 def home():
